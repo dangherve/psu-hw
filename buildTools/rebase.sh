d2e2f466dd5d65d9bdfcc5c1a71856cf5627e1cb
dangherve <dangherve@gmail.com>
2026-03-07 01:24:06 -0500

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git remote add upstream https://github.com/eez-open/psu-hw.git
+git fetch upstream
+git rebase upstream/master
+git remote set-url origin git@github.com:dangherve/psu-hw.git
+git push --force --set-upstream origin master
